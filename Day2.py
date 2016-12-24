3feac72c74d8deaf4490f74021f0b47d9a2c02d4
Christy Chen <christychen126@gmail.com>
2016-12-24 14:57:54 -0800

    Day2 part 1 Done

--- Day2.py
+++ Day2.py
@@ -13,6 +13,27 @@ def open_and_read_file(file_path):
 
 
 input_path = sys.argv[1]
-input_text_1 = open_and_read_file(input_path)
+input_text = open_and_read_file(input_path)
+input_list = input_text.split('\n')
 
+direction_dict = {
+    "U" : [ -3, "456789"],
+    "D" : [ 3, "123456"],
+    "L" : [ -1, "235689"],
+    "R" : [1, "124578"]
+    }
 
+def find_code(input_list):
+
+    door_code = []
+    starting_point = 5
+    for code in input_list:
+
+        for i in code:
+            if str(starting_point) in direction_dict[i][1]:
+                starting_point += direction_dict[i][0]
+        door_code.append(starting_point)
+
+    return door_code
+
+print find_code(input_list)
