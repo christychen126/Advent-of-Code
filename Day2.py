fdceb475fce29143ef1019221e46e07b8a892efa
Christy Chen <christychen126@gmail.com>
2016-12-24 17:00:18 -0800

    Day2 Part2 Done

--- Day2.py
+++ Day2.py
@@ -23,6 +23,7 @@ direction_dict = {
     "R" : [1, "124578"]
     }
 
+
 def find_code(input_list):
 
     door_code = []
@@ -37,3 +38,48 @@ def find_code(input_list):
     return door_code
 
 print find_code(input_list)
+
+
+
+direction_list = [ "U", "D", "L", "R"]
+code_pad = [
+    [0,0,0,0,0,0,0],
+    [0,0,0,1,0,0,0],
+    [0,0,2,3,4,0,0],
+    [0,5,6,7,8,9,0],
+    [0,0,"A","B","C",0,0],
+    [0,0,0,"D",0,0,0],
+    [0,0,0,0,0,0,0]
+]
+
+test_input = ["ULL", "RRDDD", "LURDL", "UUUUD"]
+
+
+
+def find_code_2(input_list):
+
+    door_code = []
+    starting_point = (3, 1)
+
+    for code in input_list:
+
+
+        for i in code:
+            if i == "U":
+                new_point = (starting_point[0] - 1, starting_point[1])
+            if i == "D":
+                new_point = (starting_point[0] + 1, starting_point[1])
+            if i == "L":
+                new_point = (starting_point[0], starting_point[1] - 1)
+            if i == "R": 
+                new_point = (starting_point[0], starting_point[1] + 1)
+
+            if code_pad[new_point[0]][new_point[1]] != 0:
+                starting_point = new_point
+                print starting_point
+
+        door_code.append(starting_point)
+ 
+    return door_code
+
+print find_code_2(input_list)
