5c746dad32190408facdbcff45384e7798948390
Christy Chen <christychen126@gmail.com>
2016-12-25 11:44:42 -0800

    Day3 Part2 Done

--- Day3.py
+++ Day3.py
@@ -15,6 +15,7 @@ def parsing_input(file_path):
     return new_input_list
 
 input_list = parsing_input(sys.argv[1])
+# print input_list
 
 
 
@@ -28,4 +29,38 @@ def check_triangle(input_list):
 
     return triangle_count
 
-print check_triangle(input_list)
+#print check_triangle(input_list)
+
+
+def check_triangle_2(input_list):
+    
+    triangle_count = 0
+
+    new_list_1 = []
+    new_list_2 = []
+    new_list_3 = []
+
+    for triangle in input_list:
+        print triangle
+        new_list_1.append(triangle[0])
+        new_list_2.append(triangle[1])
+        new_list_3.append(triangle[2])
+
+        # import pdb; pdb.set_trace()
+
+
+    combine_list = new_list_1 + new_list_2 + new_list_3
+
+    new_list = [combine_list[i:i+3] for i in range(0, len(combine_list), 3)]
+
+    for triangle in new_list:
+        triangle.sort()
+        if triangle[0] + triangle[1] > triangle[2]:
+            triangle_count += 1
+
+    return triangle_count
+
+
+
+print check_triangle_2(input_list)
+
